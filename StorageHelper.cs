7f6f58fcd1d6d12c95a036a7122c2fb9530bb43d
Christopher S Semler <semler.dev@outlook.com>
2026-08-05 21:19:38 -0700

    Git deleted my whole storage helper class, so im coding it again

---
+++ StorageHelper.cs
@@ -0,0 +1,70 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace VoidPulse;
+
+internal class StorageHelper
+    {
+
+    private BlobContainerClient _container;
+    private List<string> _tracks;
+    private Random _rand;
+    
+
+
+
+    public StorageHelper() 
+        {
+        _tracks = new List<string>();
+        _rand = new Random();
+       
+
+        var builder = new ConfigurationBuilder()
+        .AddUserSecrets<StorageHelper>()
+        .Build();
+
+        string sasUrl = builder["Blob:SasUrl"] ?? throw new InvalidOperationException("Missing Blob:SasUrl");
+        
+        _container = new BlobContainerClient(new Uri(sasUrl));
+
+
+        }
+
+    public async Task LoadTracksAsync()
+        {
+        _tracks.Clear();
+        
+        await foreach (BlobItem blob in _container.GetBlobsAsync())
+            {
+            if (blob.Name.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase))
+                {
+                _tracks.Add(blob.Name);
+                }
+            }
+        }
+
+    public string GetRandomTrack()
+        {
+        if (_tracks.Count == 0)
+            throw new InvalidOperationException("No tracks loaded.");
+        else
+            return _tracks[_rand.Next(_tracks.Count)];
+        }
+    public async Task<Stream> DownloadTrackAsync(string trackName)
+        {
+        BlobClient _client = _container.GetBlobClient(trackName);
+        MemoryStream _stream = new MemoryStream();
+        await _client.DownloadToAsync(_stream);
+        _stream.Position = 0;
+
+        return _stream;
+        }
+
+
+    }
+
